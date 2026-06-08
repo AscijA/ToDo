41634c1b87e8d43134e41ab8f08da993da123428
AscijA <ascija111@gmail.com>
2026-06-08 11:59:40 +0200

    Android Impl

--- ToDo.Maui.Android/MainPage.xaml.cs
+++ ToDo.Maui.Android/MainPage.xaml.cs
@@ -1,7 +1,24 @@
+using AndroidApp = global::Android.App.Application;
+
 namespace ToDo.Maui.Android;
 
 public partial class MainPage : ContentPage {
     public MainPage() {
         InitializeComponent();
+    }
+
+    protected override void OnAppearing() {
+        base.OnAppearing();
+        ApplyStatusBarPadding();
+    }
+
+    private void ApplyStatusBarPadding() {
+        var activity = Platform.CurrentActivity;
+        if (activity?.Resources?.GetIdentifier("status_bar_height", "dimen", "android") is int resourceId && resourceId > 0) {
+            var statusBarPixels = activity.Resources.GetDimensionPixelSize(resourceId);
+            var density = activity.Resources.DisplayMetrics?.Density ?? 1f;
+            var statusBarDp = statusBarPixels / density;
+            Padding = new Thickness(0, statusBarDp, 0, 0);
+        }
     }
 }
